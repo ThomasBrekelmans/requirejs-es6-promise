2183b48063c6d0b1b6843100597bdcdc51a95a27
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2015-03-18 09:40:17 +0100

    Improved README.md. Reworded JSDoc (originally taken from the RequireJS website). Add requirejs as a devDependency in bower.json (it's not actually referenced directly in the loader plugin). Bumped the version to 0.8.0.

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # requirejs-es6-promise
 A RequireJS loader plugin which allows modules to delay their availability by exporting an ES6 Promise.
+
+## TODO
+- Fix rejected promises call onLoad.error but RequireJS does not (re)throw the given error so the module depending on
+a promise prefixed resource/module is never instantiated.
+- Improve this file's Markdown.
+- Improve this file's usage section.
+- Publish this package on bower.
+
+## Usage
+- Install this package with bower (as soon as it is published)
+```sh
+bower install requirejs-es6-promise
+```
+
+- Configure the RequireJS path for this library
+@see http://requirejs.org/docs/api.html#jsfiles
+
+- Define a RequireJS (AMD) module which returns a Promise (assume this is stored in a file called async-dependency.js)
+```js
+define(function () {
+	// Pretend this module has an api whose value needs to be resolved asynchronously (perhaps it has to be loaded from 
+	// an external service?).
+	var api = {
+			value: ''
+		};
+
+	// Create a simple Promise which resolves after a timeout of 500ms (to fake an external service) or is rejected 
+	// immediately with a 50% chance.
+	var promise = new Promise(function (resolve, reject) {
+		// Do something asynchronously here and when it's finished, 
+		// call resolve with the actual value to return by this module.
+		var resolveTimeoutId = setTimeout(function () {
+			api.value = 'Resolved value set after 500ms';
+			
+			resolve(api);
+		}, 500);
+		
+		// If an error occurs,
+		if (Math.random() > 0.5) {
+			// prevent resolve from being called by aborting the timeout,
+			clearTimeout(resolveTimeoutId);
+			
+			// and call reject instead.
+			reject(new Error('Oops something went wrong'));
+		}
+	});
+	
+	return promise;
+});
+```
+
+- Define another module which depends on the first module using this plugin
+```js
+define([
+	'promise!./async-dependency'
+],
+function (
+	asyncDependency
+) {
+	// Logs 'Resolved value set after 500ms'.
+	console.log(asyncDependency.value);
+});
+```
--- es6-promise.js
+++ es6-promise.js
@@ -1,46 +1,41 @@
 /* jslint regexp: true */
 /* global require, define */
-/* global Promise */
 define(function () {
 	'use strict';
 
-	var VERSION = 0.1;
+	var VERSION = '0.8.0';
 
 	/**
-	 * A function that is called to load a resource. This is the only mandatory API method that needs to be implemented
-	 *  for the plugin to be useful.
+	 * The implementation of the RequireJS plugin load() method. This method is called automatically by RequireJS
+	 * whenever a resource is prefixed with the (aliased) name of this plugin (@see README.md#usage); the part before
+	 * the ! separator.
 	 *
-	 * @param name {String} The name of the resource to load. This is the part after the ! separator in the name.
-	 *   So, if a module asks for 'foo!something/for/foo', the foo module's load function will receive
-	 *   'something/for/foo' as the name.
-	 * @param parentRequire {Function} A local "require" function to use to load other modules. This require function
-	 *   has some utilities on it:
-	 *     parentRequire.toUrl(moduleResource) where moduleResource is a module name plus an extension. For instance
-	 *       "view/templates/main.html". It will return a full path to the resource, obeying any RequireJS configuration.
-	 *     parentRequire.defined(moduleName) Returns true if the module has already been loaded and defined.
-	 *       Used to be called require.isDefined before RequireJS 0.25.0.
-	 *     parentRequire.specified(moduleName) Returns true if the module has already been requested or is in the process
-	 *       of loading and should be available at some point.
-	 * @param onLoad {Function} A function to call with the value for name. This tells the loader that the plugin is
-	 *   done loading the resource.
-	 *     onLoad.error() can be called, passing an error object to it, if the plugin detects an error condition that
-	 *     means the resource will fail to load correctly.
-	 * @param config {Object} A configuration object. This is a way for the optimizer and the web app to pass
-	 *   configuration information to the plugin. The i18n! plugin uses this to get the current current locale, if the
-	 *   web app wants to force a specific locale. The optimizer will set an isBuild property in the config to true if
-	 *   this plugin (or pluginBuilder) is being called as part of an optimizer build.
+	 * @param name           {String}   The name of the resource to load; this is the part after the ! separator.
+	 * @param parentRequire  {Function} A local "require" function to use to load other modules.
+	 * @param onLoad         {Function} A function to call with the value for name. This tells the loader that the
+	 *                                      plugin is done loading the resource.
+	 *          onLoad.error {Function} A function to call with an error object whenever an error occurs in this plugin.
+	 * @param config         {Object}   A configuration object. This is a way for the optimizer and the web app to pass
+	 *                                    configuration information to the plugin. (Not used for this plugin.)
 	 */
 	function load (name, parentRequire, onLoad, config) {
 		// load is being called by the browser
 		parentRequire([name], function (loadedModule) {
-			// if it is not a Promise, continue normally
+			// if it is not a ('then-able') Promise
 			if (!loadedModule || !loadedModule.then) {
+				// continue normally by invoking onLoad directly (handing the responsibility to load this resource back
+				// to the default load implementation of RequireJS).
 				onLoad(loadedModule);
 				return;
 			}
 
 			// loadedModule is now assumed to be a Promise
-			// When it is resolved, call onLoad, when it is rejected, call onLoad.error.
+			// When it is resolved, call onLoad with the resolved data. When it is rejected, call onLoad.error with the
+			// given reason; the value passed to the reject method of the returned Promise.
+			// (@see MDN Promise: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
+			// TODO: onLoad.error is called when the Promise is rejected, but RequireJS doesn't rethrow the error which
+			//   causes the module which depends on a resource/module using this plugin is never instantiated and
+			//   nothing is shown to the developer.
 			loadedModule.then(onLoad, onLoad.error);
 		});
 	}
